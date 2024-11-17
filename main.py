b1f486b000905d2e579395b9e57c4219303bd107
welesi <gsf58381@gmail.com>
2024-11-17 15:08:22 +0300

    main

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+
+w =
+h =
+player_size =
+x1, y1 =
+player1_color =
+x_finish =
+
+def key_handler(event):
+    pass
+
+
+window = Tk()
+window.title("Догони меня если сможешь")
+canvas = Canvas(window, width=w, height=h, bg='white')
+canvas.pack()
+
+
+player1_id =
+finish_id =
+
+window.bind('<KeyRelease>', key_handler)
+window.mainloop()
